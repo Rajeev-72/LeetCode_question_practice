fd41647ce958b27ccf0bb6ae9e8f26721663f30a
Rajeev Mangal <rajeevmangal1510@gmail.com>
2026-05-02 16:02:05 +0530

    Added LeetCode Q4 Median of Two Sorted Arrays

---
+++ BinarySearch/median_of_two_sorted_arrays.py
@@ -0,0 +1,44 @@
+from typing import List
+
+"""
+LeetCode 4: Median of Two Sorted Arrays
+
+Approach:
+- Binary Search on smaller array
+- Time Complexity: O(log(min(n, m)))
+- Space Complexity: O(1)
+"""
+
+class Solution:
+    def findMedianSortedArrays(self, nums1: List[int], nums2: List[int]) -> float:
+        A, B = nums1, nums2
+        total = len(nums1) + len(nums2)
+        half = total // 2
+
+        if len(B) < len(A):
+            A, B = B, A 
+
+        l, r = 0, len(A) -1
+        while True:
+            i = (l + r) // 2 # A
+            j = half - i - 2 # B
+
+            Aleft = A[i] if i >= 0 else float("-infinity")
+            Aright = A[i +1] if (i+1) < len(A) else float("infinity")
+            Bleft = B[j] if j >=0 else float("-infinity")
+            Bright = B[j + 1] if (j + 1) < len(B) else float("infinity")
+
+            #  participation is correct 
+
+            if Aleft <= Bright and Bleft <= Aright:
+                # odd
+                if total %2:
+                    return min(Aright, Bright)
+
+                # even
+                return (max(Aleft, Bleft) + min(Aright, Bright)) / 2
+            elif Aleft > Bright:
+                r = i-1
+                
+            else:
+                l = i + 1 
